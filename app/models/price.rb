9ea752216c3e3c6c40c1e9360c276bb08833f409
EC2 Default User <ec2-user@ip-172-31-0-40.ap-northeast-1.compute.internal>
2021-02-23 17:19:16 +0000

    validation done

--- app/models/price.rb
+++ app/models/price.rb
@@ -1,3 +1,7 @@
 class Price < ApplicationRecord
     belongs_to :user
+    
+    validates :name, presence: true
+    validates :price, presence: true
+    validates :start_time, presence: true
 end
